f71d3791443d1ac4ad7139d9d12770615150eb6d
BttrDedThnRed <salexand@mail.umw.edu>
2022-04-13 17:19:51 -0400

    player and roster class

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+package src.src;
+
+public class Main {
+    public static void main(String[] args) {
+        Roster roster1 = new Roster("bears.txt");
+        Roster roster2 = new Roster("defenders.txt");
+        Roster roster3 = new Roster("fusion.txt");
+        Roster roster4 = new Roster("hurricanes.txt");
+        Roster roster5 = new Roster("jumbos.txt");
+        Roster roster6 = new Roster("legends.txt");
+        Roster roster7 = new Roster("rapids.txt");
+        Roster roster8 = new Roster("wolves.txt");
+
+    }
+}
--- src/Roster.java
+++ src/Roster.java
@@ -1,4 +1,33 @@
 package src.src;
-
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Roster {
+   private ArrayList<Player> roster = new ArrayList<>();
+   public Roster (String fileName) {
+      Scanner scnr = new Scanner(System.in);
+      try {
+         scnr = new Scanner (new File(fileName));
+      } catch (FileNotFoundException e){
+         System.out.println("File not found");
+         System.exit(0);
+      }
+      int count=0;
+      while (scnr.hasNextLine()) {
+         count++;
+         String playerName = scnr.nextLine();
+         String position = scnr.nextLine();
+         int playerRating = Integer.parseInt(scnr.nextLine());
+         double battingAvg = Double.parseDouble(scnr.nextLine());
+         double fieldingPct = Double.parseDouble(scnr.nextLine());
+         roster.add(new Player(playerName, position, playerRating, battingAvg, fieldingPct));
+         if (scnr.hasNextLine());
+      }
+      for (Player p:roster) {
+         System.out.println(p.getName());
+      }
+   }
+
 }
