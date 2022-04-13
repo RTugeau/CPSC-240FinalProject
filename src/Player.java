a80cbdbd27d8542e201083f73d3bf9da21489279
Cole Ely <cely@mail.umw.edu>
2022-04-13 16:16:58 -0400

    updated classes for getters and new Roster Class

--- src/Player.java
+++ src/Player.java
@@ -2,5 +2,36 @@ package src.src;
 
 public class Player {
     private String name;
+    private String position;
+    private int playerRating;
+    private double battingAvg;
+    private double fieldingPct;
 
+    public Player(String name, String position, int playerRating, double battingAvg, double fieldingPct) {
+        this.name = name;
+        this.position = position;
+        this.playerRating = playerRating;
+        this.battingAvg = battingAvg;
+        this.fieldingPct = fieldingPct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public int getPlayerRating() {
+        return playerRating;
+    }
+
+    public double getBattingAvg() {
+        return battingAvg;
+    }
+
+    public double getFieldingPct() {
+        return fieldingPct;
+    }
 }
