0d66b8a9697dae99d5b86644ab138af04b3eeb22
BttrDedThnRed <salexand@mail.umw.edu>
2022-04-18 13:50:03 -0400

    player and roster class added getTeamOverall working on GUI

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 package src.src;
+import javax.swing.*;
 
 public class Main {
+    public static JFrame frame = new JFrame("Button Example!");
     public static void main(String[] args) {
+        // make the program close when the window closes
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // create the box layout
+        frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
         Roster roster1 = new Roster("bears.txt");
         Roster roster2 = new Roster("defenders.txt");
         Roster roster3 = new Roster("fusion.txt");
@@ -11,5 +17,26 @@ public class Main {
         Roster roster7 = new Roster("rapids.txt");
         Roster roster8 = new Roster("wolves.txt");
         System.out.println(roster1.getOverall());
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void addButton(String text, JFrame f) {
+        // add a button object
+        JButton button = new JButton(text);
+        button.addActionListener(new ButtonListener(text));
+        f.getContentPane().add(button);
+
+    }
+    public static void simOrTradeWindow(String rosterName) {
+        frame.setVisible(false);
+        JFrame frame2 = new JFrame("Sim or Trade Window");
+        // make the program close when the window closes
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // create the box layout
+        frame2.getContentPane().setLayout(new BoxLayout(frame2.getContentPane(), BoxLayout.Y_AXIS));
+        addButton("Trade", frame2);
+        addButton("Game Simulator", frame2);
+        frame2.pack();
+
     }
 }
--- src/Trade.java
+++ src/Trade.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Trade {
     public static void trade() {
         Scanner scnr = new Scanner(System.in);
-        System.out.println("What players would you like to trade? ");
+        System.out.println("What team would you like to trade?");
+        String answer = scnr.nextLine();
+
 
     }
 }
